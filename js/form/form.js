93d2bb9e0b24ed05d89b428211d147f8b3879cb1
Mykhailo <97468374+UlanovychM@users.noreply.github.com>
2024-11-10 19:44:34 +0200

    start new project

---
+++ js/form/form.js
@@ -0,0 +1,11 @@
+const form = () => {
+  const form = document.querySelector('.form');
+  const containerWorkouts = document.querySelector('.workouts');
+  const inputType = document.querySelector('.form__input--type');
+  const inputDistance = document.querySelector('.form__input--distance');
+  const inputDuration = document.querySelector('.form__input--duration');
+  const inputCadence = document.querySelector('.form__input--temp');
+  const inputElevation = document.querySelector('.form__input--climb');
+};
+
+export default form;
---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import form from './form/form';
+import map from './map/map';
+
+window.addEventListener('DOMContentLoaded', () => {
+  form();
+  map();
+});
