134f769c707a160f694fcd68d9593236bd02e452
Mykhailo <97468374+UlanovychM@users.noreply.github.com>
2024-11-11 15:51:39 +0200

    'update'

--- js/map/map.js
+++ js/map/map.js
@@ -1,3 +1,29 @@
-const map = () => {};
+const map = () => {
+  const map = L.map('map').setView([50.44, , 30.51], 15.25);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        // const { latitude } = position.coords;
+        // const { longitude } = position.coords;
+        // console.log(position);
+        // console.log(
+        //   `https://www.google.com/maps/@50.441665,30.5149715,15.25z?entry=ttu&g_ep=EgoyMDI0MTEwNi4wIKXMDSoASAFQAw%3D%3D`
+        // );
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+
+        L.marker([50.44, , 30.51])
+          .addTo(map)
+          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          .openPopup();
+      },
+      () => {
+        alert(`check your privacy settings`);
+      }
+    );
+  }
+};
 
 export default map;
